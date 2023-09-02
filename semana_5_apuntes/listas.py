1225296e0b8200e38caf8246d568f271e63639a7
erremalave <90105673+erremalave@users.noreply.github.com>
2023-09-02 11:29:23 -0500

    Update listas.py
    se agregan nuevas listas y un ciclo for para mostrar la lista de estatura

--- semana_5_apuntes/listas.py
+++ semana_5_apuntes/listas.py
@@ -1,2 +1,10 @@
 #ejemplo de lista
 nombre = ["Pepito", "Andres", "Juan", "Maria", "Pedro"]
+edad = [25, 19, 21, 33, 40]
+estatura = [1.80, 1.65, 1.74, 1.66, 1.54]
+casado = [False, False, False, True, True]
+usuario = ["Pepito", 25, 1.80, False]
+
+#ciclo for para mostrar lista
+for i in range(len(estatura)):
+    print(estatura[i])
